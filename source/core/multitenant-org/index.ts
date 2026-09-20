e0a407c826516a4e2802cddf91001e729ff0d688
roney ruiz <roneyruizrojas@gmail.com>
2026-09-20 12:47:30 -0600

    chore(harness): adjust barrel export and layering rule

--- source/core/multitenant-org/index.ts
+++ source/core/multitenant-org/index.ts
@@ -1,10 +1,9 @@
-export { Service } from "./contexts/service.aggregate";
-export { DuplicateServiceNameError } from "./contexts/service.errors";
-export { ServiceId } from "./contexts/service.vos";
-export { Tenant } from "./contexts/tenant.aggregate";
-export type { TenantPlan } from "./contexts/tenant.enums";
-export {
-	PlanDowngradeConflictError,
-	ServiceCapExceededError,
-} from "./contexts/tenant.errors";
-export { TenantId } from "./contexts/tenant.vos";
+export * from "./contexts/service.aggregate";
+export * from "./contexts/service.errors";
+export * from "./contexts/service.types";
+export * from "./contexts/service.vos";
+export * from "./contexts/tenant.aggregate";
+export * from "./contexts/tenant.enums";
+export * from "./contexts/tenant.errors";
+export * from "./contexts/tenant.types";
+export * from "./contexts/tenant.vos";
